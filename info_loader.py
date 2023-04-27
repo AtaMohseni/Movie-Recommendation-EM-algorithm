81c7f75e7c0e2113740a70c50621c0ae2d2d221a
AtaMohseni <amohseni@ucsd.edu>
2023-04-27 14:14:26 -0700

    added two functions to info_loader.py. dunction MovieList() to read and return list of movies, and UserID() to read and return user IDs

--- info_loader.py
+++ info_loader.py
@@ -6,4 +6,27 @@ Created on Wed Apr 26 15:24:25 2023
 """
 
 def MovieList():
-    pass
+    try:
+        fhand = open("./hw8_movieTitles_fa18.txt")
+    except:
+        print ('the file does not exist')
+        return None
+    movie_list = []
+    for line in fhand:
+        movie_list.append(line.strip())
+    return movie_list
+    
+def UserID():
+    try:
+        fhand = open("./hw8_studentPIDs_fa18.txt")
+    except:
+        print ('the file does not exist')
+        return None
+    
+    User_ids =[]
+    for line in fhand:
+        User_ids.append(line.strip())
+    return User_ids
+
+
+        
