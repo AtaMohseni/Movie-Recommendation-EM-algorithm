f159df32303e0b3c8c2bc8ed99aad9274d6658d7
AtaMohseni <amohseni@ucsd.edu>
2023-04-29 15:58:06 -0700

    added Movie_Recommendation.py file and added Movie_recommend() class with its initialization method

---
+++ Movie_Recommendation.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 29 14:55:49 2023
+
+@author: ATA
+"""
+
+class Movie_Recommend():
+    
+    def __init__(self,movie_list,number_of_clusters):
+        
+        #list of movies for rating and recommendation
+        self.movie_list = movie_list
+        # number of moviegoer groups to project user into these groups 
+        self.number_of_clusters = int(number_of_clusters)
+        # initial probability of user belongs to moviegoer groups
+        self.probZ = [1/self.number_of_clusters for n in range(self.number_of_clusters)]
+        #probability of user likes a movie knowing that she/he belongs to 
+        #any of moviegoer groups
+        import numpy as np
+        self.probRgivenZ = np.random.random_sample((len(self.movie_list),self.number_of_clusters))
+        
+        
